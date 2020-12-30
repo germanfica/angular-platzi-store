38e62d9edff3f2af40c35e1012b23977ee610769
German Fica <germafica@gmail.com>
2020-12-30 19:04:00 -0300

    Componente product y Decorador Product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Product } from './modules/product.module';
 
 @Component({
   selector: 'app-root',
@@ -10,52 +9,6 @@ export class AppComponent {
   title = 'platzi-store';
   isVisible = true;
   veganFoods = ['🍎', '🍏', '🍇', '🍌', '🍑'];
-
-  products: Product[] = [
-    {
-      id: '1',
-      image: 'assets/images/camiseta.png',
-      title: 'Camiseta',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '2',
-      image: 'assets/images/hoodie.png',
-      title: 'Hoodie',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '3',
-      image: 'assets/images/mug.png',
-      title: 'Hoodie',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '4',
-      image: 'assets/images/pin.png',
-      title: 'Pin',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '5',
-      image: 'assets/images/stickers1.png',
-      title: 'Stickers 1',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '6',
-      image: 'assets/images/stickers2.png',
-      title: 'Stickers 2',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    }
-  ];
-
 
   addItem(){
     this.veganFoods.push('New item')
