e1d88b2d29fbce4e7e34b58e054c1cc7b4056642
German Fica <germafica@gmail.com>
2020-12-30 02:50:56 -0300

    Lista de productos sin estilos

---
+++ src/app/modules/product.module.ts
@@ -0,0 +1,8 @@
+// Esto protege a nuestro código, evitando tipear mal
+export interface Product {
+    id: string;
+    image: string;
+    title: string;
+    price: number;
+    description: string;
+}
