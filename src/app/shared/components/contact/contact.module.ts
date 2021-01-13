cb3d5dc67771fca85ef828e95d565f6ef9022916
German Fica <germafica@gmail.com>
2021-01-13 19:29:00 -0300

    Form module for Contact

--- src/app/shared/components/contact/contact.module.ts
+++ src/app/shared/components/contact/contact.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ContactRoutingModule } from './contact-routing.module';
 
 import { ContactComponent } from './contact.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     ContactComponent,
+    FormComponent,
   ],
   imports: [
     CommonModule,
