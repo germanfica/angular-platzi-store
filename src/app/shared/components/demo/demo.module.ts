4bedda858cbc80629a3469379655a16457d6b4d2
German Fica <germafica@gmail.com>
2021-01-13 03:12:18 -0300

    Deliting space blanks

--- src/app/shared/components/demo/demo.module.ts
+++ src/app/shared/components/demo/demo.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DemoRoutingModule } from './demo-routing.module';
 
-
-
 @NgModule({
   declarations: [
     DemoModule
