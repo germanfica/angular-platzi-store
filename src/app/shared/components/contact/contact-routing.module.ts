51f86cece533698bf0ac994c0578295c2ac2b34e
German Fica <germafica@gmail.com>
2021-01-13 19:47:10 -0300

    Contact new folder structure

--- src/app/shared/components/contact/contact-routing.module.ts
+++ src/app/shared/components/contact/contact-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { ContactComponent } from './contact.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 import { AdminGuard } from './../../../admin/admin.guard'
 
--- src/app/shared/components/contact/contact.module.ts
+++ src/app/shared/components/contact/contact.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactRoutingModule } from './contact-routing.module';
 
-import { ContactComponent } from './contact.component';
-import { FormComponent } from './form/form.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FormComponent } from './components/form/form.component';
 
 @NgModule({
   declarations: [
