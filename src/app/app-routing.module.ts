4135399fb4de2ccba2ea4f398142f76e0ac19f0b
German Fica <germafica@gmail.com>
2021-01-14 18:02:27 -0300

    Removing an unneeded import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
-import { PageNotFoundComponent } from './page-not-found/components/page-not-found/page-not-found.component';
 import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [
