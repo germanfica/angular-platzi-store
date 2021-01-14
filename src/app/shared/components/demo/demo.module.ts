0446536584293432ee901bb015d428c79f61c4fa
German Fica <germafica@gmail.com>
2021-01-14 01:45:57 -0300

    Optimizing core.module

--- src/app/shared/components/demo/demo.module.ts
+++ src/app/shared/components/demo/demo.module.ts
@@ -6,16 +6,13 @@ import { FormsModule } from '@angular/forms';
 import { DemoComponent } from './demo.component';
 
 import { SharedModule } from '../../shared.module';
-import { CoreModule } from 'src/app/core/core.module';
 
 @NgModule({
   declarations: [
     DemoComponent,
-    // ExponentialPipe,
   ],
   imports: [
     CommonModule,
-    CoreModule,
     SharedModule,
     DemoRoutingModule,
     RouterModule,
