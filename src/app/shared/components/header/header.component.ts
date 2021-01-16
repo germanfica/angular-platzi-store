8e9ee23eb6955c19eff7a9d485c3d981b49bf12c
German Fica <germafica@gmail.com>
2021-01-16 06:10:03 -0300

    sideHeaderDrawer

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MatDrawer } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  // @Input() sideHeaderDrawer: any;
+  @Input() sideHeaderDrawer: MatDrawer = {} as MatDrawer;
   
   constructor() { }
 
