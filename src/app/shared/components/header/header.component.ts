f501abed6b9451541ec15378143a5542f66dc1f0
German Fica <germafica@gmail.com>
2021-01-16 23:36:41 -0300

    updateIsMobile

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
 import { MatDrawer } from '@angular/material/sidenav';
 
 @Component({
@@ -9,10 +9,28 @@ import { MatDrawer } from '@angular/material/sidenav';
 export class HeaderComponent implements OnInit {
   // @Input() sideHeaderDrawer: any;
   @Input() sideHeaderDrawer: MatDrawer = {} as MatDrawer;
-  
+  isMobile: boolean = false;
+  innerWidth: number = 0;
+
+  // https://stackoverflow.com/questions/45350716/detecting-real-time-window-size-changes-in-angular-4
+  @HostListener('window:resize')
+  onResize() {
+    this.innerWidth = window.innerWidth;
+    this._updateIsMobile();
+  }
+
+  _updateIsMobile() {
+    if (innerWidth <= 768) { // 768px portrait
+      this.isMobile = true;
+    } else {
+      this.isMobile = false;
+    }
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+    this._updateIsMobile();
   }
 
 }
