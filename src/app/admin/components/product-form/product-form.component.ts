890d3057fc629c3e87059e83c382837a5e014e4f
German Fica <germafica@gmail.com>
2021-01-30 07:53:40 -0300

    Create product

--- src/app/admin/components/product-form/product-form.component.ts
+++ src/app/admin/components/product-form/product-form.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Product } from '@core/models/product';
+
+import { ProductsService } from '@core/services/products/products.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-form',
@@ -10,13 +14,27 @@ export class ProductFormComponent implements OnInit {
   form: FormGroup;
 
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private productsService: ProductsService,
+    private router: Router
   ) {
     this.form = {} as FormGroup;
     this.buildForm();
   }
 
   ngOnInit(): void {
+  }
+
+  createProduct(event: Event) {
+    event.preventDefault();
+    if(this.form.valid) {
+      const product: Product = this.form.value;
+      this.productsService.createProduct(product).subscribe((p) => {
+        console.log(p);
+        console.log(this.form.value);
+        this.router.navigate(['./admin/products']);
+      });
+    }
   }
 
   private buildForm() {
@@ -28,5 +46,4 @@ export class ProductFormComponent implements OnInit {
       description: ['', [Validators.required]],
     });
   }
-
 }
