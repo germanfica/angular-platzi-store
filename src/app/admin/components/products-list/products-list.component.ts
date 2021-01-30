695fa9a3592a533a9806a244546d4a323e21fb6e
German Fica <germafica@gmail.com>
2021-01-30 04:08:37 -0300

    Comments update

--- src/app/admin/components/products-list/products-list.component.ts
+++ src/app/admin/components/products-list/products-list.component.ts
@@ -29,11 +29,11 @@ export class ProductsListComponent implements OnInit {
   }
 
   /**
-   * Genera una nueva `lista de productos` pero ignorando el producto especificado
-   * por parámetro. Esto sigue siendo ineficiente. Lo ideal sería mantener una
+   * Genera una nueva `lista de productos` ignorando el producto eliminado.
+   * Esto sigue siendo ineficiente. Lo ideal sería mantener una
    * lista de productos con una cierta cantidad de productos, por ejemplo
    * máximo 10 productos (con un Paginator se solucionaria).
-   * @param id Identificador del producto que se quiere ignorar.
+   * @param id Identificador del producto eliminado que se quiere ignorar.
    */
   filterProducts(id: string) {
     this.products = this.products.filter(product => product.id !== id);
