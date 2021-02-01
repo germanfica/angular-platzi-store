2d0f6d3363d3f13184cfc2ff984d7204aa7429c0
German Fica <germafica@gmail.com>
2021-02-01 02:55:18 -0300

    Fixing contact page

--- src/app/contact/components/form/form.component.ts
+++ src/app/contact/components/form/form.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-form',
+  selector: 'app-contact-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.form = {} as FormGroup;
+    this.buildForm();
+  }
 
   ngOnInit(): void {
   }
 
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      fullname:['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      content: ['', [Validators.required]],
+    });
+  }
+
+  sendMessage(event: Event) {
+    event.preventDefault();
+  }
 }
