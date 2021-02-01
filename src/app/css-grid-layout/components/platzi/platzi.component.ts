df9977b53602a850a53c6f1feef3f8768585cc7f
German Fica <germafica@gmail.com>
2021-02-01 03:04:24 -0300

    Fixing Platzi Page Form

--- src/app/css-grid-layout/components/platzi/platzi.component.ts
+++ src/app/css-grid-layout/components/platzi/platzi.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Platzi } from '@core/models/platzi';
 
 @Component({
@@ -7,14 +8,31 @@ import { Platzi } from '@core/models/platzi';
   styleUrls: ['./platzi.component.scss']
 })
 export class PlatziComponent implements OnInit {
-  platzi: Platzi = {
-    id: '1',
-    image: 'assets/images/platzi/react-js/badge.png'
-  };
+  form: FormGroup;
+  platzi: Platzi;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.form = {} as FormGroup;
+    this.platzi = {
+      id: '1',
+      image: 'assets/images/platzi/react-js/badge.png'
+    };
+    this.buildForm();
+  }
 
   ngOnInit(): void {
   }
 
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      name:['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  startPlatzi(event: Event) {
+    event.preventDefault();
+  }
 }
