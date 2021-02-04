6938fae5d0bd06e98ab31fa55372298380fb9810
German Fica <germafica@gmail.com>
2021-02-04 03:47:54 -0300

    Creando la página de la orden y uso de async

--- src/app/order/components/order/order.component.ts
+++ src/app/order/components/order/order.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Product } from '@core/models/product';
 import { CartService } from '@core/services/cart/cart.service';
 import { Observable } from 'rxjs';
@@ -10,13 +11,27 @@ import { Observable } from 'rxjs';
 })
 export class OrderComponent implements OnInit {
   products$!: Observable<Product[]>;
+  isLinear = true;
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+
   constructor(
-    private cartService: CartService
+    private _cartService: CartService,
+    private _formBuilder: FormBuilder
   ) {
-    this.products$ = this.cartService.cart$;
+    this.products$ = this._cartService.cart$;
+    this.firstFormGroup = {} as FormGroup;
+    this.secondFormGroup = {} as FormGroup;
   }
 
   ngOnInit(): void {
+    this.firstFormGroup = this._formBuilder.group({
+      // podria crear un Validator para products.length === 0
+      firstCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
   }
 
 }
--- src/app/order/order.module.ts
+++ src/app/order/order.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { OrderRoutingModule } from './order-routing.module';
 import { OrderComponent } from './components/order/order.component';
+import { MaterialModule } from '@app/material/material.module';
+import { SharedModule } from '@shared/shared.module';
 
 
 @NgModule({
   declarations: [OrderComponent],
   imports: [
     CommonModule,
+    SharedModule,
+    MaterialModule,
     OrderRoutingModule
   ]
 })
